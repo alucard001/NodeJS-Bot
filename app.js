248f851d952030e195daeceb1d18db414b6d75c7
Ellery Leung <alucard001@gmail.com>
2018-03-20 11:56:54 +0800

    Use HTTPS as link prefix

--- app.js
+++ app.js
@@ -73,10 +73,10 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             var card = new builder.HeroCard(session)
                 .title('1. 選取你想購買的身體檢查及按「購買」')
                 .images([
-                    builder.CardImage.create(session, 'http://203.184.176.136/bot/1.png')
+                    builder.CardImage.create(session, 'https://www.lksf.org/bot/1.png')
                 ])
                 .buttons([
-                    builder.CardAction.openUrl(session, 'http://203.184.176.136/bot/1.png', '放大')
+                    builder.CardAction.openUrl(session, 'https://www.lksf.org/bot/1.png', '放大')
                 ]);
             var msg = new builder.Message(session).addAttachment(card);
             session.send(msg);
@@ -85,10 +85,10 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             // var card = new builder.HeroCard(session)
             //     .title('2. 選擇你的免費及附加項目 (適用於有相關選項的身體檢查)')
             //     .images([
-            //         builder.CardImage.create(session, 'http://203.184.176.136/bot/2.png')
+            //         builder.CardImage.create(session, 'https://www.lksf.org/bot/2.png')
             //     ])
             //     .buttons([
-            //         builder.CardAction.openUrl(session, 'http://203.184.176.136/bot/2.png', '放大')
+            //         builder.CardAction.openUrl(session, 'https://www.lksf.org/bot/2.png', '放大')
             //     ]);
             // var msg = new builder.Message(session).addAttachment(card);
             // session.send(msg);
@@ -97,10 +97,10 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             // var card = new builder.HeroCard(session)
             //     .title('3. 選擇你的免費禮品(適用於有相關選項的身體檢查)，之後按「確認」至下一頁填寫資料。如你想選購其他產品，可先按「儲存繼續選購」。')
             //     .images([
-            //         builder.CardImage.create(session, 'http://203.184.176.136/bot/3.png')
+            //         builder.CardImage.create(session, 'https://www.lksf.org/bot/3.png')
             //     ])
             //     .buttons([
-            //         builder.CardAction.openUrl(session, 'http://203.184.176.136/bot/3.png', '放大')
+            //         builder.CardAction.openUrl(session, 'https://www.lksf.org/bot/3.png', '放大')
             //     ]);
             // var msg = new builder.Message(session).addAttachment(card);
             // session.send(msg);
